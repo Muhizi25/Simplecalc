d29594c8f7192aea767daba832552d03ad026dcb
Muhizi Mozaya <Muhizimozaya@gmail.com>
2022-03-25 13:10:33 +0300

    New game

--- script.js
+++ script.js
@@ -1,25 +1,55 @@
-let num1=8
-let num2 =2
-document.getElementById("num1-el").textContent=num1
-document.getElementById("num2-el").textContent=num2
-    let sumEl = document.getElementById("sum-el")
-let Additon = num1+num2
-let subtruction = num1-num2
-let Devision = num1/num2
-let Multiplication = num1*num2
-function Add(){
-    Additon
-    sumEl.textContent = "OuPut ="+ Additon
+let firstCard = getRandomCard()
+let secondCard = getRandomCard()
+let sum = firstCard+secondCard
+let blackJack = false
+let isAlive = true
+let message = ""
+ let messageEl = document.getElementById("message-el")
+ let sumEl = document.getElementById("sum-el")
+ let cardEl = document.getElementById("card-el")
+let cards =[firstCard,secondCard]
+
+
+function getRandomCard(){
+     
+   let rondomnum = Math.floor( Math.random()*13) + 1
+   if(rondomnum > 10){
+       return 10
+   }else if(rondomnum === 1){
+       return 11
+   }else{
+       return rondomnum
+   }
+ }
+ getRandomCard()
+
+ function startGame(){
+    renderGame()
+ }
+function renderGame(){
+    cardEl.textContent = "Card:"
+    for(let i =0; i<cards.length;i++){
+        cardEl.textContent += cards[1] + "  "
+    }
+    sumEl.textContent = "Sum:"+ ""+ sum
+    
+   
+    if(sum<=20){
+        message = "Do you want to draw a new card?"
+    
+    }else if(sum===21){
+        message = " You've got Blackjack!"
+        blackJack = true
+    }else{
+        message = "You're out of the game!"
+        isAlive = true
+    }
+    messageEl.textContent = message
 }
-function Subtruct(){
-    subtruction
-    sumEl.textContent = "OuPut ="+ subtruction
+function newCard(){
+    let card = getRandomCard()
+    sum += card
+    cards.push(card)
+    renderGame()
 }
-function Divede(){
-    Devision
-    sumEl.textContent = "OuPut ="+  Devision
-}
-function Multply(){
-    Multiplication
-    sumEl.textContent = "OuPut ="+ Multiplication
-}
+
